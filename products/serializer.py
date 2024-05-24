4b7a4a4999dbb8e7efab5d2072e63c8053fc4a92
amirsallah <amir1381basketball@gmail.com>
2024-05-24 18:48:21 +0330

    feat: add url to product serializer

--- products/serializer.py
+++ products/serializer.py
@@ -6,19 +6,19 @@ from .models import Product, Category, File
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('title', 'description', 'avatar')
+        fields = ('id', 'title', 'description', 'avatar')
 
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ('title', 'file')
+        fields = ('id', 'title', 'file')
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     category = CategorySerializer(many=True)
     files = FileSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = ('title', 'description', 'avatar', 'category', 'files')
+        fields = ('url', 'id', 'title', 'description', 'avatar', 'category', 'files')
