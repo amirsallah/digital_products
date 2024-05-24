b23a7473f3a3ccd52785d0f507176e089a699fd3
amirsallah <amir1381basketball@gmail.com>
2024-05-24 18:46:09 +0330

    feat: add id to admin panel

--- products/admin.py
+++ products/admin.py
@@ -5,20 +5,20 @@ from products.models import Product, Category, File
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['parent', 'title', 'is_active', 'created_time']
+    list_display = ['id', 'parent', 'title', 'is_active', 'created_time']
     list_filter = ['parent', 'is_active']
     search_fields = ['title']
 
 
 class FileInlineAdmin(admin.TabularInline):
     model = File
-    fields = ['title', 'file', 'is_active']
+    fields = ['id', 'title', 'file', 'is_active']
     extra = 0
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'is_active', 'created_time']
+    list_display = ['id', 'title', 'is_active', 'created_time']
     list_filter = ['is_active']
     filter_horizontal = ['category']
     inlines = [FileInlineAdmin]
